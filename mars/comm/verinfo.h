ea033ea8e49ee8bccb74360f1eaebd546e1e0c34
Vast Gui <sakurajimamai2020@qq.com>
2024-06-20 09:51:49 +0800

    update(xlog):移除了Android日志打印的文件头内容，只显示日志本体内容

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "2b0aa80a"
+#define MARS_REVISION "c69582ab"
 #define MARS_PATH "canary"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-06-19 00:36:33"
+#define MARS_BUILD_TIME "2024-06-20 09:49:38"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1718728593
+#define MARS_BUILD_TIMESTAMP 1718848178
 
 #endif
